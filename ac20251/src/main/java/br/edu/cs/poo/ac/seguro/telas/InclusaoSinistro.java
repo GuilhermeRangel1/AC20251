acb83b5bcbed8a0f25945d081f9a1a7ca85283f1
GuilhermeRangelSilva <162377684+GuilhermeRangel1@users.noreply.github.com>
2025-05-31 10:22:14 -0300

    tela CRUD SeguradoPessoa funcionando

--- ac20251/src/main/java/br/edu/cs/poo/ac/seguro/telas/InclusaoSinistro.java
+++ ac20251/src/main/java/br/edu/cs/poo/ac/seguro/telas/InclusaoSinistro.java
@@ -6,7 +6,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class TelaSeguradoEmpresaCRUD extends JFrame {
+public class InclusaoSinistro extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
@@ -18,7 +18,7 @@ public class TelaSeguradoEmpresaCRUD extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					TelaSeguradoEmpresaCRUD frame = new TelaSeguradoEmpresaCRUD();
+					InclusaoSinistro frame = new InclusaoSinistro();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -30,7 +30,7 @@ public class TelaSeguradoEmpresaCRUD extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public TelaSeguradoEmpresaCRUD() {
+	public InclusaoSinistro() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
